f5c47631941020adc7762f70d0bdff840046c5e7
x20064Naruse <yoy11029mc@gmail.com>
2022-03-15 08:09:12 +0900

    windowオーバーレイまで

--- python/test.py
+++ python/test.py
@@ -1,55 +1,41 @@
-#!python3
-# encoding:utf-8
+#!/usr/bin/python
+ 
+# Import PySide classes
+import sys
+from PySide.QtCore import *
+from PySide.QtGui import *
+ 
 
-from PySide2 import QtWidgets
-from PySide2 import QtCore
-from PySide2 import QtGui
+class App:
+  def __init__(self):
+    # Create a Qt application
+    self.app = QApplication(sys.argv)
+    
+    icon = QIcon("jenkins_favicon.png")
+    menu = QMenu()
+    settingAction = menu.addAction("setting")
+    settingAction.triggered.connect(self.setting)
+    exitAction = menu.addAction("exit")
+    exitAction.triggered.connect(sys.exit)
+    
+    self.tray = QSystemTrayIcon()
+    self.tray.setIcon(icon)
+    self.tray.setContextMenu(menu)
+    self.tray.show()
+    self.tray.setToolTip("unko!")
+    self.tray.showMessage("hoge", "moge")
+    self.tray.showMessage("fuga", "moge")
+    
+  def run(self):
+    # Enter Qt application main loop
+    self.app.exec_()
+    sys.exit()
 
-class Window(QtWidgets.QMainWindow):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.setWindowFlags(QtCore.Qt.Tool|QtCore.Qt.FramelessWindowHint|QtCore.Qt.WindowStaysOnTopHint)
-
-        # 撮影用に位置を変更
-        self.setGeometry(1400, 850, 200, 200)
-
-        label = QtWidgets.QLabel("Frame Less Window Sample", self)
-        label.setGeometry(0, 0, 200, 200)
-        label.setStyleSheet("background-color: #222; color: #EEE;")
-
-class TaskTray_Icon(QtWidgets.QSystemTrayIcon):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        menu = QtWidgets.QMenu()
-
-        quit_action = menu.addAction("Quit")
-        quit_action.triggered.connect(self.__quit)
-
-        # コンテキストメニューに作成したメニューをセット
-        self.setContextMenu(menu)
-
-        # NOTE: アイコンには通常はpngなどを指定するが、
-        # サンプルなのでPixmapでもモックを作ってお茶を濁す。
-        # icon = QtGui.QIcon('icon.png')
-        pixmap = QtGui.QPixmap(QtCore.QSize(32, 32))
-        pixmap.fill(QtGui.QColor("red"))
-        icon = QtGui.QIcon(pixmap)
-
-        self.setIcon(icon)
-
-    def __quit(self):
-        QtWidgets.QApplication.quit()
-
+  def setting(self):
+    self.dialog = QDialog()
+    self.dialog.setWindowTitle("Setting Dialog")
+    self.dialog.show()
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication()
-
-    # トレイアイコンを生成して表示。
-    trayicon = TaskTray_Icon()
-
-    trayicon.show()
-
-    window = Window()
-    window.show()
-    exit(app.exec_())
+  app = App()
+  app.run()
