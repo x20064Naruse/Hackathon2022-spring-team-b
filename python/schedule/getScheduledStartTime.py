2007fc05cfa55eeceada14823fc597d7155321db
KotaHigashimoto <kotahigashimoto@gmail.com>
2022-03-15 12:29:03 +0900

    calc remaining time

--- python/schedule/getScheduledStartTime.py
+++ python/schedule/getScheduledStartTime.py
@@ -10,9 +10,9 @@ from google.oauth2.credentials import Credentials
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 
-def main():
-    """Google Calendar APIの基本的な使い方を説明します。
-    ユーザーのカレンダーにある次の10件のイベントの開始と名前を表示する。
+def getScheduledStartTime():
+    """
+    ユーザーのカレンダーにある次の1件のイベントの開始と名前を表示
     """
     creds = None
     # token.jsonというファイルには、ユーザーのアクセストークンやリフレッシュトークンが保存され、
@@ -27,7 +27,7 @@ def main():
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
+        # 次の実行のために認証情報を保存
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
@@ -37,28 +37,19 @@ def main():
     now = datetime.datetime.utcnow().isoformat() + 'Z'
     print('Getting the upcoming 1 events')
     events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=1, singleEvents=True,
-                                        orderBy='startTime').execute() #maxResults:直近イベント件数
+                                          maxResults=1, singleEvents=True,
+                                          orderBy='startTime').execute()  # maxResults:直近イベント件数
     events = events_result.get('items', [])
-
-    targetChar1 = 'T'
-    targetChar2 = '+'
-    
 
     if not events:
         print('No upcoming events found.')
     for event in events:
-        #start = event['start'].get('dateTime', event['start'].get('date'))
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        #print('StartTime:', start, '\n','EventName:', event['summary'])
-        str=start
-        idx = str.find(targetChar2)
-        str2 = str[:idx]        # +よりも前を抽出
-        target = 'time: '  
-        idx = str2.find(targetChar1)    # Tより後ろを抽出
-        out = str2[idx+len(targetChar1):]
-        print(out)
+        start = event['start'].get('dateTime', event['start'].get('dateTime'))
+        # print('StartTime:', start, '\n','EventName:', event['summary'])
 
+        # UNIXTIMEへ変換
+        uptoSecond = start[:start.find('+')]  # '+'よりも前を抽出
+        d = datetime.datetime.strptime(uptoSecond, '%Y-%m-%dT%H:%M:%S')  # string -> datetime
+        scheduledUNIX = d.timestamp()  # datetime -> UNIXTIME
 
-if __name__ == '__main__':
-    main()
+    return scheduledUNIX
