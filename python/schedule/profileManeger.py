4c75ffc4a90c6bd34c226b70679c6bb975a1ce4a
KotaHigashimoto <kotahigashimoto@gmail.com>
2022-03-17 14:55:26 +0900

    プロファイル保存と読込まで

--- python/schedule/profileManeger.py
+++ python/schedule/profileManeger.py
@@ -6,8 +6,6 @@ import time
 import os.path
 
 # プロファイル構造体
-
-
 @dataclass
 class ProfileList:
     game_title: str  # ゲームタイトル
@@ -16,19 +14,27 @@ class ProfileList:
     priority: int  # 優先度
     quantity: int  # 最大値
 
-# プロファイル作成
 
+# ゲームタイトルを指定してプロファイル保存
+def saveProfile(updateRequestProfile,game_title):
+    # loadedProfiles = loadProfile(game_title)  # プロファイル読込
 
-def initProfile(game_title, task_name, required_time, priority, quantity):
-    m = ProfileList(game_title, task_name, required_time, priority, quantity)
+    # # プロファイル書換
+    # for uP in updateRequestProfiles:
+    #     for lP in loadedProfiles:
+    #         if uP.game_title == lP.game_title and uP.task_name == lP.task_name:
+    #             # 既存のプロファイルは更新
+    #             lP.required_time = uP.required_time
+    #             lP.priority = uP.priority
+    #             lP.quantity = uP.quantity
+    #         else:
+    #             # 新規のプロファイルは追加
+    #             lP.append(uP)
 
-
-# プロファイル最適化&保存
-def optimizeProfile(game_title, task_name, required_time):
-    obj = loadProfile(game_title)  # プロファイル読込
-
-    pandas.to_pickle(obj, "profiles.pkl")  # オブジェクト保存
-    print('[ProfileManeger] Optimized.')
+    newProfile = updateRequestProfile
+    filename = game_title + '.pkl'
+    pandas.to_pickle(newProfile, filename)  # プロファイル保存
+    print('[ProfileManeger] Saved to',filename)
 
     # # プロファイルが存在しない場合はそのまま保存
     # if not obj:
@@ -42,18 +48,32 @@ def optimizeProfile(game_title, task_name, required_time):
     #     print('[ProfileManeger] Saved.')
 
 
-# プロファイル読込
+# # プロファイル最適化&保存（保留）
+# def optimizeProfile(game_title, task_name, required_time):
+#     obj = loadProfile(game_title)  # プロファイル読込
+
+#     pandas.to_pickle(obj, "profiles.pkl")  # オブジェクト保存
+#     print('[ProfileManeger] Optimized.')
+
+#     # # プロファイルが存在しない場合はそのまま保存
+#     # if not obj:
+#     #     init = []
+#     #     init[0] = ProfileList(game_title, task_name,required_time, priority, quantity)
+#     #     pandas.to_pickle(obj, "profiles.pkl")  # オブジェクト保存
+#     #     print('[ProfileManeger] Initialized.')
+#     # # プロファイルが存在する場合は中身を更新
+#     # else:
+#     #     pandas.to_pickle(obj, "profiles.pkl")  # オブジェクト保存
+#     #     print('[ProfileManeger] Saved.')
+
+
+# ゲームタイトルを指定してプロファイル読込
 def loadProfile(game_title):
-    # プロファイル読込
-    # obj = pandas.read_pickle("profiles.pkl")  # オブジェクト読出
-    print('[ProfileManeger] Loaded.')
+    filename=game_title+'.pkl'
+    if not os.path.exists(filename):
+        print('[ProfileManeger]',filename,'is not found.')
 
-    # ダミーデータ
-    p = []
-    p.append(ProfileList("Apex", "1Round", 30, 1, 0))
-    p.append(ProfileList("Apex", "DailyChallenge", 2, 3, 5))
-    p.append(ProfileList("Apex", "3V3", 10, 2, 0))
-    p.append(ProfileList("Apex", "WeeklyChallenge", 10, 4, 11))
-    profile = p
+    loadedProfiles = pandas.read_pickle(filename)  # オブジェクト読出
+    print('[ProfileManeger] Loaded from',filename)
 
-    return profile
+    return loadedProfiles
