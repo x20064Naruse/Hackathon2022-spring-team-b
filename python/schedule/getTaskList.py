bf9457d6e5c166552c147671aea5ca2c62a694c3
KotaHigashimoto <kotahigashimoto@gmail.com>
2022-03-17 10:03:05 +0900

    スケジュール提案の関数化まで

--- python/schedule/getTaskList.py
+++ python/schedule/getTaskList.py
@@ -8,62 +8,46 @@ from profileManeger import loadProfile
 from profileManeger import optimizeProfile
 from taskListManeger import updateTaskList
 
-
-def getTaskList():
-
-    game_title = "Apex"
+# スケジュール提案
+def getTaskList(game_title):
 
     # 予定開始時刻取得 (UNIX)
     scheduledUNIX = int(getScheduledStartTime())
 
-    # アプリ終了まで無限ループ
-    while True:
+    # 現在時刻取得 (UNIX)
+    nowUNIX = int(time.time())
+    # print('now:', nowUNIX)
+    # print('scheduled:', scheduledUNIX)
 
-        # 予定開始時刻取得 (UNIX) 注意：1sec毎に取得するとラグい
-        # scheduledUNIX = int(getScheduledStartTime())
+    # 残り時間計算(sec)
+    remainSec = scheduledUNIX-nowUNIX
+    # print(remainSec, 'seconds left')
+    if remainSec > 0:
+        print(datetime.timedelta(seconds=remainSec))
 
-        # 表示切替ON
-        toggleButton = True
-        if (toggleButton):
+    # # チェック入力
+    # checkBoxListener = True
+    # if checkBoxListener:
+    #     # 必要時間最適化
+    #     # 現在時刻取得 (UNIX)
+    #     checkboxUNIX = int(time.time())
+    #     # タスク名取得
+    #     task_name = "3V3"
+    #     required_time = 5
+    #     optimizeProfile(game_title, task_name, required_time)
+    # else:
+    #     print('checkBoxListener == False')
 
-            # 現在時刻取得 (UNIX)
-            nowUNIX = int(time.time())
-            # print('now:', nowUNIX)
-            # print('scheduled:', scheduledUNIX)
+    # # Update List
+    # updatedTaskList = updateTaskList(remainSec, game_title)
+    TaskList = updateTaskList(remainSec, game_title)
 
-            # 残り時間計算(sec)
-            remainSec = scheduledUNIX-nowUNIX
-            # print(remainSec, 'seconds left')
-            if remainSec > 0:
-                print(datetime.timedelta(seconds=remainSec))
+    # # Show List
+    # if not updatedTaskList:
+    #     print('リストはありません.\n')
+    # else:
+    #     for l in updatedTaskList:
+    #         print('-', l.task_name, '[', l.required_time, 'm]')
+    #     print('\n')
 
-            # チェック入力
-            checkBoxListener = True
-            if checkBoxListener:
-                # 必要時間最適化
-                # 現在時刻取得 (UNIX)
-                checkboxUNIX = int(time.time())
-                # タスク名取得
-                task_name = "3V3"
-                required_time = 5
-                optimizeProfile(game_title, task_name, required_time)
-            else:
-                print('checkBoxListener == False')
-
-            # Update List
-            updatedTaskList = updateTaskList(remainSec, game_title)
-
-            # Show List
-            if not updatedTaskList:
-                print('リストはありません.\n')
-            else:
-                for l in updatedTaskList:
-                    print('-', l.task_name, '[', l.required_time, 'm]')
-                print('\n')
-
-        # 表示切替OFF
-        else:
-            print('toggleButton == False')
-
-        # 1sec待つ
-        time.sleep(1)
+    return TaskList
--- python/schedule/main.py
+++ python/schedule/main.py
@@ -6,6 +6,7 @@ import os.path
 from getScheduledStartTime import getScheduledStartTime
 from profileManeger import loadProfile
 from profileManeger import optimizeProfile
+from getTaskList import getTaskList
 from taskListManeger import updateTaskList
 
 
@@ -13,60 +14,16 @@ def main():
 
     game_title = "Apex"
 
-    # 予定開始時刻取得 (UNIX)
-    scheduledUNIX = int(getScheduledStartTime())
+    # Update List
+    updatedTaskList = getTaskList(game_title)
 
-    # アプリ終了まで無限ループ
-    while True:
-
-        # 予定開始時刻取得 (UNIX) 注意：1sec毎に取得するとラグい
-        # scheduledUNIX = int(getScheduledStartTime())
-
-        # 表示切替ON
-        toggleButton = True
-        if (toggleButton):
-
-            # 現在時刻取得 (UNIX)
-            nowUNIX = int(time.time())
-            # print('now:', nowUNIX)
-            # print('scheduled:', scheduledUNIX)
-
-            # 残り時間計算(sec)
-            remainSec = scheduledUNIX-nowUNIX
-            # print(remainSec, 'seconds left')
-            if remainSec > 0:
-                print(datetime.timedelta(seconds=remainSec))
-
-            # チェック入力
-            checkBoxListener = True
-            if checkBoxListener:
-                # 必要時間最適化
-                # 現在時刻取得 (UNIX)
-                checkboxUNIX = int(time.time())
-                # タスク名取得
-                task_name = "3V3"
-                required_time = 5
-                optimizeProfile(game_title, task_name, required_time)
-            else:
-                print('checkBoxListener == False')
-
-            # Update List
-            updatedTaskList = updateTaskList(remainSec, game_title)
-
-            # Show List
-            if not updatedTaskList:
-                print('リストはありません.\n')
-            else:
-                for l in updatedTaskList:
-                    print('-', l.task_name, '[', l.required_time, 'm]')
-                print('\n')
-
-        # 表示切替OFF
-        else:
-            print('toggleButton == False')
-
-        # 1sec待つ
-        time.sleep(1)
+    # Show List
+    if not updatedTaskList:
+        print('リストはありません.\n')
+    else:
+        for l in updatedTaskList:
+            print('-', l.task_name, '[', l.required_time, 'm]')
+        print('\n')
 
 
 if __name__ == '__main__':
